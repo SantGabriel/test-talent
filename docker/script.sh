adc82deb2069e8ce6cc77c09f532f5f557609833
Gabriel <gabrielsantana@gmail.com>
2025-11-04 18:18:25 -0300

    docker and scripts

--- docker/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker exec -it web-talent chown -R 1000:www-data storage bootstrap/cache
+docker exec -it web-talent chmod -R 775 storage bootstrap/cache
+docker exec -it web-talent php artisan migrate
+
+docker exec -it mysql-talent mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS talent CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
