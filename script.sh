bf499f30f52788143200b330747d39a5d1a56dca
Gabriel <gabrielsantana@gmail.com>
2025-11-14 17:16:25 -0300

    docker script env

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker exec -it web-talent chown -R 1000:1000 ./
-docker exec -it web-talent chown -R 1000:www-data storage bootstrap/cache
-docker exec -it web-talent chmod -R 775 storage bootstrap/cache
-docker exec -it web-talent php artisan migrate
+docker exec -it mysql-talent mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS talent CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+docker exec -it mysql-talent mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS talent_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
-docker exec -it mysql-talent mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS talent CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-docker exec -it mysql-talent mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS talent_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+docker exec -it web-talent composer install
+docker exec -it web-talent cp .env.example .env
+docker exec -it web-talent php artisan migrate
+docker exec -it web-talent php artisan db:seed
